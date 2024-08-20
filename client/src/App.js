faa8e54318a940fa0d17249404f132a81a51f199
Toshiaki Iizuka <t_112ka@yahoo.co.jp>
2024-08-20 16:36:43 +0900

    feat: add AppBar with design

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,51 @@
-import React from "react";
+import React from 'react';
+import { Typography, AppBar } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+import VideoPlayer from './components/VideoPlayer';
+import Notifications from './components/Notifications';
+import Options from './components/Options';
+
+const StyledAppBar = styled(AppBar)(({ theme }) => ({
+  borderRadius: 15,
+  margin: '30px 100px',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '600px',
+  border: '2px solid black',
+
+  [theme.breakpoints.down('xs')]: {
+    width: '90%',
+  },
+}));
+
+const Wrapper = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  width: '100%',
+});
+
+const Image = styled('img')({
+  marginLeft: '15px',
+});
 
 const App = () => {
-	return <div>App</div>;
+  return (
+    <Wrapper>
+      <StyledAppBar position='static' color='inherit'>
+        <Typography variant='h2' align='center'>
+          Video Chat
+        </Typography>
+      </StyledAppBar>
+      <VideoPlayer />
+      <Options>
+        <Notifications />
+      </Options>
+    </Wrapper>
+  );
 };
 
 export default App;
