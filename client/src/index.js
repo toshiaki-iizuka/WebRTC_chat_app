87670569d1f2ce6ea6ba47e9c0f91d45c1f66a46
Toshiaki Iizuka <t_112ka@yahoo.co.jp>
2024-08-20 16:53:51 +0900

    feat: add calling user

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
+import { ContextProvider } from './SocketContext';
 import './styles.css';
 
-createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById('root')).render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>,
+);
