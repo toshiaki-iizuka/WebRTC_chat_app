1baaa18b36c9cb274bb45994ddd3f091e40b1213
Toshiaki Iizuka <t_112ka@yahoo.co.jp>
2024-08-20 17:31:30 +0900

    feat: add video iframe

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -1,7 +1,54 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Grid, Typography, Paper } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+import { SocketContext } from '../SocketContext';
+
+const Video = styled('video')(({ theme }) => ({
+  width: '550px',
+  [theme.breakpoints.down('xs')]: {
+    width: '300px',
+  },
+}));
+
+const GridContainer = styled(Grid)(({ theme }) => ({
+  justifyContent: 'center',
+  [theme.breakpoints.down('xs')]: {
+    flexDirection: 'column',
+  },
+}));
+
+const StyledPaper = styled(Paper)({
+  padding: '10px',
+  border: '2px solid black',
+  margin: '10px',
+});
 
 const VideoPlayer = () => {
-  return <div>VideoPlayer</div>;
+  const { name, callAccepted, myVideo, userVideo, callEnded, call } = useContext(SocketContext);
+
+  return (
+    <GridContainer container>
+      <StyledPaper>
+        <Grid item xs={12} md={6}>
+          <Typography variant='h5' gutterBottom>
+            {name || 'Name'}
+          </Typography>
+          <Video playsInline muted ref={myVideo} autoPlay />
+        </Grid>
+      </StyledPaper>
+      {callAccepted && !callEnded && (
+        <StyledPaper>
+          <Grid item xs={12} md={6}>
+            <Typography variant='h5' gutterBottom>
+              {call.name || 'Name'}
+            </Typography>
+            <Video playsInline ref={userVideo} autoPlay />
+          </Grid>
+        </StyledPaper>
+      )}
+    </GridContainer>
+  );
 };
 
 export default VideoPlayer;
